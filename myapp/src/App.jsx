53497e1a179b77da0658d204b6d7d180f79184d5
sonukalakar <ajaypandat329@gmail.com>
2022-09-29 19:37:19 +0530

    day3

---
+++ myapp/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Routes } from "./Components/Route/Routes"
+
+const App = () => {
+
+  return (
+    <div>
+      <Routes />
+    </div>
+  );
+}
+
+
+export default App;
